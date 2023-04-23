f2e0130d114d0f1f857e83665851515db643a1dc
lyssah4 <isimbilyssah003@gmail.com>
2023-04-23 04:46:13 -0700

    update

---
+++ handle_print.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * handle_print - prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed
+ * @ind: ind
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculate active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size:
+ * Return:
